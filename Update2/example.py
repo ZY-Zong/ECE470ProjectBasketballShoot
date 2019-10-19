d1d16e1e2d28156cbb34f8e6d1622982f52feb48
ZY-Zong <44796784+ZY-Zong@users.noreply.github.com>
2019-10-18 23:04:35 -0500

    Add files via upload

---
+++ Update2/example.py
@@ -0,0 +1,183 @@
+import vrep
+import time
+import numpy as np
+from math import cos, sin
+from scipy.linalg import expm,logm
+
+
+# Get distances measurements from each joint center to base frame (useful for forward kinematics)
+def get_joint():
+	X = []
+	Y = []
+	Z = []
+	result,vector=vrep.simxGetObjectPosition(clientID, joint_one_handle,base_handle,vrep.simx_opmode_blocking)
+	X.append(vector[0])
+	Y.append(vector[1])
+	Z.append(vector[2])
+	result,vector=vrep.simxGetObjectPosition(clientID, joint_two_handle,base_handle,vrep.simx_opmode_blocking)
+	X.append(vector[0])
+	Y.append(vector[1])
+	Z.append(vector[2])
+	result,vector=vrep.simxGetObjectPosition(clientID, joint_three_handle,base_handle,vrep.simx_opmode_blocking)
+	X.append(vector[0])
+	Y.append(vector[1])
+	Z.append(vector[2])
+	result,vector=vrep.simxGetObjectPosition(clientID, joint_four_handle,base_handle,vrep.simx_opmode_blocking)
+	X.append(vector[0])
+	Y.append(vector[1])
+	Z.append(vector[2])
+	result,vector=vrep.simxGetObjectPosition(clientID, joint_five_handle,base_handle,vrep.simx_opmode_blocking)
+	X.append(vector[0])
+	Y.append(vector[1])
+	Z.append(vector[2])
+	result,vector=vrep.simxGetObjectPosition(clientID, joint_six_handle,base_handle,vrep.simx_opmode_blocking)
+	X.append(vector[0])
+	Y.append(vector[1])
+	Z.append(vector[2])
+	result,vector=vrep.simxGetObjectPosition(clientID, end_handle,base_handle,vrep.simx_opmode_blocking)
+	X.append(vector[0])
+	Y.append(vector[1])
+	Z.append(vector[2])
+	X = np.round(X, decimals = 3)
+	Y = np.round(Y, decimals = 3)
+	Z = np.round(Z, decimals = 3)
+	return X,Y,Z
+
+# Function that used to move joints
+def SetJointPosition(theta):
+	vrep.simxSetJointTargetPosition(clientID, joint_one_handle, theta[0], vrep.simx_opmode_oneshot)
+	time.sleep(0.5)
+	vrep.simxSetJointTargetPosition(clientID, joint_two_handle, theta[1], vrep.simx_opmode_oneshot)
+	time.sleep(0.5)
+	vrep.simxSetJointTargetPosition(clientID, joint_three_handle, theta[2], vrep.simx_opmode_oneshot)
+	time.sleep(0.5)
+	vrep.simxSetJointTargetPosition(clientID, joint_four_handle, theta[3], vrep.simx_opmode_oneshot)
+	time.sleep(0.5)
+	vrep.simxSetJointTargetPosition(clientID, joint_five_handle, theta[4], vrep.simx_opmode_oneshot)
+	time.sleep(0.5)
+	vrep.simxSetJointTargetPosition(clientID, joint_six_handle, theta[5], vrep.simx_opmode_oneshot)
+	time.sleep(0.5)
+
+# Function that used to read joint angles
+def GetJointAngle():
+	result, theta1 = vrep.simxGetJointPosition(clientID, joint_one_handle, vrep.simx_opmode_blocking)
+	if result != vrep.simx_return_ok:
+		raise Exception('could not get 1 joint variable')
+	result, theta2 = vrep.simxGetJointPosition(clientID, joint_two_handle, vrep.simx_opmode_blocking)
+	if result != vrep.simx_return_ok:
+		raise Exception('could not get 2 joint variable')
+	result, theta3 = vrep.simxGetJointPosition(clientID, joint_three_handle, vrep.simx_opmode_blocking)
+	if result != vrep.simx_return_ok:
+		raise Exception('could not get 3 joint variable')
+	result, theta4 = vrep.simxGetJointPosition(clientID, joint_four_handle, vrep.simx_opmode_blocking)
+	if result != vrep.simx_return_ok:
+		raise Exception('could not get 4 joint variable')
+	result, theta5 = vrep.simxGetJointPosition(clientID, joint_five_handle, vrep.simx_opmode_blocking)
+	if result != vrep.simx_return_ok:
+		raise Exception('could not get 5 joint variable')
+	result, theta6 = vrep.simxGetJointPosition(clientID, joint_six_handle, vrep.simx_opmode_blocking)
+	if result != vrep.simx_return_ok:
+		raise Exception('could not get 6 joint variable')
+	theta = np.array([[theta1],[theta2],[theta3],[theta4],[theta5],[theta6]])
+	return theta
+
+
+
+# ======================================================================================================= #
+# ======================================= Start Simulation ============================================== #
+# ======================================================================================================= #
+
+# Close all open connections (Clear bad cache)
+vrep.simxFinish(-1)
+# Connect to V-REP (raise exception on failure)
+clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+if clientID == -1:
+	raise Exception('Failed connecting to remote API server')
+
+# ======================================== Setup "handle"  =========================================== #
+
+'''
+# Print object name list
+result,joint_name,intData,floatData,stringData = vrep.simxGetObjectGroupData(clientID,vrep.sim_appobj_object_type,0,vrep.simx_opmode_blocking)
+print(stringData)
+'''
+
+# Get "handle" to the base of robot
+result, base_handle = vrep.simxGetObjectHandle(clientID, 'UR3_link1_visible', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for base frame')
+    
+# Get "handle" to the all joints of robot
+result, joint_one_handle = vrep.simxGetObjectHandle(clientID, 'UR3_joint1', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for first joint')
+result, joint_two_handle = vrep.simxGetObjectHandle(clientID, 'UR3_joint2', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for second joint')
+result, joint_three_handle = vrep.simxGetObjectHandle(clientID, 'UR3_joint3', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for third joint')
+result, joint_four_handle = vrep.simxGetObjectHandle(clientID, 'UR3_joint4', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for fourth joint')
+result, joint_five_handle = vrep.simxGetObjectHandle(clientID, 'UR3_joint5', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for fifth joint')
+result, joint_six_handle = vrep.simxGetObjectHandle(clientID, 'UR3_joint6', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for sixth joint')
+
+# Get "handle" to the end-effector of robot
+result, end_handle = vrep.simxGetObjectHandle(clientID, 'UR3_link7_visible', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get object handle for end effector')
+# ==================================================================================================== #
+
+# Start simulation
+vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
+
+# ******************************** Your robot control code goes here  ******************************** #
+time.sleep(1)
+
+home = np.radians([167.22, -73.48, 70.54, -86.48, -89.78, 47.20])
+
+Q11 = np.radians([155.82, -52.67, 86.82, -122.89, -89.2, 35.68])
+Q12 = np.radians([156.07, -47.04, 87.12, -128.81, -89.2, 36.01])
+Q13 = np.radians([156.06, -40.99, 87.99, -135.74, -89.2, 36.09])
+
+Q21 = np.radians([171.8, -51.51, 86.13, -123.39, -88.86, 51.66])
+Q22 = np.radians([171.77, -46.69, 88.03, -130.12, -88.86, 51.73])
+Q23 = np.radians([171.82, -41.76, 87.79, -134.81, -88.86, 51.83])
+
+Q31 = np.radians([187.66, -45.44, 74.48, -117.94, -88.53, 67.46])
+Q32 = np.radians([187.64, -41.64, 76.28, -123.55, -88.53, 67.53])
+Q33 = np.radians([187.63, -36.09, 77.17, -129.98, -88.56, 67.6])
+
+Q = [ [Q11, Q12, Q13], \
+      [Q21, Q22, Q23], \
+      [Q31, Q32, Q33] ]
+
+
+Goal_joint_angles = np.array([[0,0.5*np.pi,-0.5*np.pi,0.5*np.pi,-0.5*np.pi,np.pi], \
+							  [-0.5*np.pi,0,-0.5*np.pi,0,0.5*np.pi,-0.5*np.pi],\
+							  [0.5*np.pi,-0.5*np.pi,-0.5*np.pi,0,0,-0.5*np.pi]])
+for i in range(3):
+	SetJointPosition(Goal_joint_angles[i])
+	time.sleep(2)
+    
+
+# Wait two seconds
+time.sleep(2)
+# **************************************************************************************************** #
+
+# Stop simulation
+vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
+# Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
+vrep.simxGetPingTime(clientID)
+# Close the connection to V-REP
+vrep.simxFinish(clientID)
+print("==================== ** Simulation Ended ** ====================")
+
+# ======================================================================================================= #
+# ======================================== End Simulation =============================================== #
+# ======================================================================================================= #
