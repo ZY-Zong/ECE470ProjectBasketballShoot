dc6d4539a210a19ef9d1577cabd0e5de44214972
zhenyuz2 <zhenyuz2@illinois.edu>
2019-11-17 21:26:05 -0600

    checkpoint4

--- Update4/update4.py
+++ Update4/update4.py
@@ -39,38 +39,37 @@ def forward_k(theta):
 # ======================================================================================================== #
 grap_state = 0
 # ========================================= Jaco Hand function =========================================== #
-def JacoHandGrap():
-	global grap_state
+def JacoHandGrasp():
+	vrep.simxSetStringSignal(clientID,'jacoHand','true',vrep.simx_opmode_oneshot)
+	time.sleep(1)
+	print("Try to grasp.")
+	return
 
-	# input
-	input_invalid = 1
-	while(input_invalid):
-		input_msg = input("grap or release? (please enter grap or release)")
-		if((input_msg=="grap") | (input_msg=="release")):
-			input_invalid = 0
-		else:
-			print("invalid input!\n")
-	
-	# grap
-	if (input_msg == "grap"):
-		if (grap_state == 0):
+def JacoHandRelease():
+	vrep.simxSetStringSignal(clientID,'jacoHand','false',vrep.simx_opmode_oneshot)
+	time.sleep(1)
+	print("Release item.")
+	return
 
-			vrep.simxSetStringSignal(clientID,'jacoHand','true',vrep.simx_opmode_oneshot)
-			time.sleep(1)
-			grap_state = 1
+def JacoHandHasItem():
+	# result, state, force, torque = vrep.simxReadForceSensor(clientID, jacohand_force_sensor_handle, vrep.simx_opmode_streaming)
+	# time.sleep(0.1)
+	# result, state, force, torque = vrep.simxReadForceSensor(clientID, jacohand_force_sensor_handle, vrep.simx_opmode_buffer)
+	# print('############')
+	# print(result)
+	# print(state)
+	# print(force)
+	# print(torque)
+	# print('############')
 
-		else:
-			print("Already grapped. Ignore your command.")
-			return
+	# print('############')
+	ret, state, arr1, value, arr2 = vrep.simxReadProximitySensor(clientID, proximity_sensor_handle, vrep.simx_opmode_streaming)
+	time.sleep(0.1)
+	ret, state, arr1, value, arr2 = vrep.simxReadProximitySensor(clientID, proximity_sensor_handle, vrep.simx_opmode_buffer)
+	# print(ret, state, arr1, value, arr2)
+	# print('############')
 
-	# release
-	if (input_msg == "release"):
-		if (grap_state == 1):
-			vrep.simxSetStringSignal(clientID,'jacoHand','false',vrep.simx_opmode_oneshot)
-			grap_state = 0
-		else:
-			print("Already release. Ignore your command.")
-			return
+	return state
 
 # ======================================================================================================== #
 
@@ -197,6 +196,17 @@ result, joint_six_handle = vrep.simxGetObjectHandle(clientID, 'UR3_joint6', vrep
 if result != vrep.simx_return_ok:
 	raise Exception('could not get object handle for sixth joint')
 
+# Get "handle" to the force sensor of JacoHand
+result, jacohand_force_sensor_handle = vrep.simxGetObjectHandle(clientID, 'JacoHand_forceSens2_finger1', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get force sensor for the first jacohand finger')
+result, jacohand_joint2_finger1 = vrep.simxGetObjectHandle(clientID, 'JacoHand_joint2_finger1', vrep.simx_opmode_blocking)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get joint2 for the first jacohand finger')
+result, proximity_sensor_handle = vrep.simxGetObjectHandle(clientID, 'Proximity_sensor', vrep.simx_opmode_oneshot_wait)
+if result != vrep.simx_return_ok:
+	raise Exception('could not get proximity sensor')
+
 # Get "handle" to the end-effector of robot
 result, end_handle = vrep.simxGetObjectHandle(clientID, 'UR3_link7_visible', vrep.simx_opmode_blocking)
 if result != vrep.simx_return_ok:
@@ -207,21 +217,39 @@ if result != vrep.simx_return_ok:
 vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
 
 # ******************************** Your robot control code goes here  ******************************** #
-#time.sleep(1)
+time.sleep(1)
 
 pi = np.pi
-Goal_joint_angles = np.array([0.,0.,0.,0.,0.,0.])
+Goal_joint_angles = np.array([-pi/2,pi/2.,0.,0.,0.,pi/2])
 SetJointPosition(Goal_joint_angles)
+SetJointPosition(np.array([0.,0.,0.,0.,0.,pi/2]))
 
 # Wait two seconds
-time.sleep(1)
+time.sleep(3)
 # *************************************************************************************************** #
 
+command = np.array([0,0,0,0,0,0])
 
-command = np.array([0.,0.,0.,0.,0.,0.])
 #close the Jaco hand at first#
 vrep.simxSetStringSignal(clientID,'jacoHand','false',vrep.simx_opmode_oneshot)
-# j=0
+j=0
+
+# Decision making
+hasItem = 0
+while(hasItem == 0):
+	if (JacoHandHasItem()):
+		hasItem = 1
+		time.sleep(3)
+		JacoHandGrasp()
+		time.sleep(1)
+		SetJointPosition(np.array([-pi/2,pi/2.,0.,0.,0.,pi/2]))
+		JacoHandRelease()
+	print("No item, wait!")
+	time.sleep(1)
+
+time.sleep(2)
+
+# Forward kinematics
 # while(j<10):
 # 	invalid = 1
 # 	while(invalid):
@@ -243,10 +271,11 @@ vrep.simxSetStringSignal(clientID,'jacoHand','false',vrep.simx_opmode_oneshot)
 # 	j+=1
 # print("your 10 chances are over, simulation will stop\n")
 
-j = 0;
-inc1 = 1.1;
-inc2 = 0.4;
-inc3 = 0.3;
+# Throw items
+j = 0
+inc1 = 1.1
+inc2 = 0.4
+inc3 = 0.3
 while(j<7):
 	command[1] -= float(15*inc1)*pi/float(180)
 	command[2] -= float(5*inc2)*pi/float(180)
@@ -259,6 +288,7 @@ while(j<7):
 
 while(1):
 	inc1 = 1
+
 
 # Stop simulation
 vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
